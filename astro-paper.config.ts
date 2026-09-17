b7627029526c9d6dbe1f57c180f701504ffb1a76
Eason <eason@example.com>
2026-09-17 11:36:51 +0800

    minor fix

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -24,7 +24,6 @@ export default defineAstroPaperConfig({
     showBackButton: true,
     editPost: {
       enabled: false,
-      url: "https://github.com/easonxiang/blog_astro/blob/master/",
     },
     search: "pagefind",
   },
